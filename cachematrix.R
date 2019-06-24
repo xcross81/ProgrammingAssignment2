b2907a96fa6b215fb5624f5c1244956974b75196
xcross81 <calebrese@gmail.com>
2019-06-24 17:30:01 -0400

    cacheSolve implemented and tested

--- cachematrix.R
+++ cachematrix.R
@@ -1,27 +1,9 @@
-## Creates a list with function that can set the matrix,
-## show the matrix
-## and set/show the matrix inverse
-##
-## When the matrix is set, the inverse (if set) is cleared
-## in order to prevent a wrong inverse value from staying stored
-## The value function shows the value of the stored matrix
-## 
-## The inverse function will return the value of the
-## stored inverse or set(and return) the value of the inverse
-## if the inverse hasn't already been assigned. The
-## default argument value for the inverse function is NULL.
-##
-## Usage example
-## specialmatrix <- makeCacheMatrix()
-## matrixdata <- matrix(c(1:9),nrow=3,ncol=3)
-## specialmatrix$set(matrixdata)
-## specialmatrix$value()
-## specialmatrix$inverse(inversedata)
+## Used for creating a matrix that stores its inverse
+## Includes function for calculating and setting inverse
 
 
-
-## Write a short comment describing this function
-
+##Creates a list with functions that can set the matrix,
+## show the matrix, and set/show the matrix inverse
 makeCacheMatrix <- function(x = matrix()) {
   
   invx <- NULL          #sets stored inverse null
@@ -46,8 +28,43 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## cacheSolve takes in a matrix made from an object
+## untilizing makeCacheMatrix, returns the matrix inverse
+## from cache if already calculated and cached, otherwise
+## calculates inverse and caches
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+
+  inverse <- x$inverse()
+  if(!is.null(inverse)){
+    message("Using cached data")
+    inverse
+  }
+  else{
+    matrixdata <- x$value()
+    inverse <- solve(matrixdata)
+    x$inverse(inverse)
+    inverse
+  }
+  
+  
 }
+
+####makeCacheMatrix addition notes
+## When the matrix is set, the inverse (if set) is cleared
+## in order to prevent a wrong inverse value from staying stored
+## The value function shows the value of the stored matrix
+## 
+## The inverse function will return the value of the
+## stored inverse or set(and return) the value of the inverse
+## if the inverse hasn't already been assigned. The
+## default argument value for the inverse function is NULL.
+
+## Usage example
+## specialmatrix <- makeCacheMatrix()
+## matrixdata <- matrix(c(1,8,5,7,9,3,2,4,3),nrow=3,ncol=3)
+## specialmatrix$set(matrixdata)
+## specialmatrix$value()
+## specialmatrix$inverse(inversedata)
+## cacheSolve(specialmatrix)
